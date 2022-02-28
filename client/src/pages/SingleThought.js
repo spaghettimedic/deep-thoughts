738a03629d08eb2c9aab11b67a802738e8e434b7
spaghettimedic <dsteiner247@gmail.com>
2022-02-28 15:37:09 -0500

    implement addThought mutation, implement addReaction form and mutation

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -30,6 +32,7 @@ const SingleThought = props => {
         </div>
       </div>
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
